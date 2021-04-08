b0baacafa44d7f6666b0c38d17aecfb252622c70
ii masamitsu <ii@entap.co.jp>
2021-04-08 14:43:17 +0900

    最初のサービスプロバイダーを作成

---
+++ src/CarpServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Entap\Laravel\Carp;
+
+use Illuminate\Support\ServiceProvider;
+
+class CarpServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
